eb60481929a3d58911cf902c04b3f1f72364ad68
Paul Cortes <paulkerg@gmail.com>
2020-05-03 01:03:52 -0500

    se agrega el cambio user id en el tareasReducer

--- src/reducers/tareasReducer.js
+++ src/reducers/tareasReducer.js
@@ -1,18 +1,24 @@
-import { TRAER_TODAS, CARGANDO, ERROR } from "../types/tareasTypes";
+import {
+  TRAER_TODAS,
+  CARGANDO,
+  ERROR,
+  CAMBIO_USER_ID,
+} from "../types/tareasTypes";
 
 const INITIAL_STATE = {
   tareas: {},
   cargando: false,
   error: "",
-  usuario_id: '4',
-  titulo: 'qwerty'
+  usuario_id: "4",
+  titulo: "qwerty",
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case TRAER_TODAS:
       return { ...state, tareas: action.payload, cargando: false, error: "" };
-
+    case CAMBIO_USER_ID:
+      return { ...state, tareas: action.payload, cargando: false, error: "" };
     case CARGANDO:
       return { ...state, cargando: true };
     case ERROR:

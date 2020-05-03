d401c5d588a09cf7a100787f9e9512cda2944b37
Paul Cortes <paulkerg@gmail.com>
2020-05-03 00:51:16 -0500

    Se crea formulario de crear tarea

--- src/components/Tareas/Guardar.js
+++ src/components/Tareas/Guardar.js
@@ -3,7 +3,18 @@ import React, { Component } from 'react'
  class Guardar extends Component {
   render() {
     return (
-     <div>Guardar</div>
+     <div>
+       <h1>Guardar Tarea</h1>
+       Usuario id:
+       <input type="number"/>
+       <br/><br/>
+       Titulo:
+       <input type="text"/>
+       <br/><br/>
+       <button>
+         Guardar
+       </button>
+     </div>
     )
   }
 }
